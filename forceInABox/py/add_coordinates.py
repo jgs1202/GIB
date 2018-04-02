97320c5247cb1b6dc322c0128b3bec3dc5e00d40
jgs1202 <jgs.kuee@gmail.com>
2018-04-02 13:25:14 +0900

    update

---
+++ forceInABox/py/add_coordinates.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+import os
+import json
+
+main = '../data/CGIB/'
+output = '../data/CGIB_comp/'
+
+inp = input('Are you really run this program? This can damage your data. (y/n) :')
+if inp == 'y':
+    for dir in os.listdir(main):
+        if dir != '.DS_Store':
+            for num in range(20):
+                f = open(main + dir + '/' + str(num) + '-nodes.txt')
+                txt = f.read()
+                reader = open(main + dir + '/' + str(num) + '.json')
+                global data
+                data = json.load(reader)
+
+                length = len(data['nodes'])
+                list = [i for i in range(length)]
+                sentence = ''
+                num = 0
+                index = 0
+                for i in txt:
+                    try:
+                        sentence += (str(int(i)))
+                    except:
+                        if i == '.':
+                            sentence = sentence + '.'
+                        else:
+                            if num == 0:
+                                global dic
+                                dic = {}
+                                dic['cx'] = float(sentence)
+                                sentence = ''
+                                num += 1
+                            elif num == 1:
+                                dic['cy'] = float(sentence)
+                                # dic['index'] = index
+                                sentence = ''
+                                num = 0
+                                list[index] = dic
+                                index += 1
+                            else:
+                                print(error)
+
+                for i in range(length):
+                    data['nodes'][i]['cx'] = list[data['nodes'][i]['index']]['cx']
+                    data['nodes'][i]['cy'] = list[data['nodes'][i]['index']]['cy']
+
+                try:
+                    current = os.listdir(output + dir)
+                except:
+                    os.mkdir(output + dir)
+                f = open(output + dir + '/' + str(num) + '.json' , 'w')
+                json.dump(data, f, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
+else:
+    pass
