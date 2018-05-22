a82c5c7dc94af9bdd1c0639fbfc66b200a3f8fe8
Nozomi Aoyama <jgs.kuee@gmail.com>
2018-05-22 12:46:26 +0900

    update

---
+++ comp1.0/py/put50.py
@@ -0,0 +1,34 @@
+import os
+import json
+from setAnswer import calc
+
+def arrange():
+    pathes = []
+    pathes.append('../data/FDGIB/comp/')
+    # pathes.append('../data/TRGIB/comp/')
+    # pathes.append('../data/Chaturvedi/comp/')
+    # pathes.append('../data/STGIB/comp/')
+
+    for output in pathes:
+        if os.path.exists(output) != True:
+            os.mkdir(output)
+
+    count = 0
+    num = 0
+
+    for path in pathes:
+        if os.path.exists(path + str(num) + '.json'):
+            count += 1
+        else:
+            print(num)
+        num += 1
+        if count != 0 and count % 50 == 0:
+            num = 60 * (floor( num / 60) + 1)
+
+    
+
+def main():
+    arrange()
+
+if __name__ == '__main__':
+    main()
